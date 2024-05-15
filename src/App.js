691a86821bdea1c82b1adc70275b994f19b96a4f
patidaranand2001 <anandpatidat603@gmail.com>
2024-05-15 10:38:55 +0530

    cra tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='bg-red-500'>hello from netflix-gpt
+       </h1>
     </div>
   );
 }
